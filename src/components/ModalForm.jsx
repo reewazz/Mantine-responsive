8656ca737314aa2369af18701f1df88257d592ea
riktriz <riwaj.neupane15@gmail.com>
2024-05-21 16:34:57 +0545

    updated

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -1,4 +1,4 @@
-import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import { useDisclosure } from "@mantine/hooks";
 import { Modal, Button, TextInput, Textarea, Group, Box } from "@mantine/core";
 
 export const ModalForm = () => {
@@ -15,18 +15,19 @@ export const ModalForm = () => {
         transitionProps={{ transition: "fade", duration: 200 }}
       >
         <Box className="modalbox flex flex-col lg:flex-row gap-4 lg:gap-20 justify-center ">
-          <div className="resume w-full  lg:w-3/5 lg:h-full ">
-            <div className="buttons flex gap-4 sm:gap-10 mb-3">
-              <Button variant="filled">Resume</Button>
-              <Button variant="filled">Cover</Button>
+          <div className="resume w-full  lg:w-1/2 ">
+            <div className="buttons flex gap-4 sm:gap-10 mb-3 ">
+              <Button variant="filled"> Resume </Button>
+              <Button variant="filled"> Cover </Button>
             </div>
+
             <img
-              className="w-full h-auto   "
-              src="https://resumaker.ai/s3/en-US/cv-examples/Data-Scientist-CV-Example.png"
-              alt=""
+              className="w-full h-auto"
+              src="https://resumaker.ai/s3/en-US/cv-examples/Software-Engineer-CV-Example.png"
+              alt="Software Engineer CV"
             />
           </div>
-          <div className="remarks mt-6 lg:mt-6 w-full lg:w-1/4">
+          <div className="remarks mt-6 lg:mt-7 w-full lg:w-1/4">
             <p className="font-bold">Remarks*</p>
             <form className="space-y-4 p-4 border-2">
               {Array.from({ length: 10 }).map((_, index) => (
